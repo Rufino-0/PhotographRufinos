51220737a99b209d7a794c442efb5e7f683ac957
Rufino-0 <eli.rufino@sptech.school>
2024-05-24 09:38:18 -0300

    Inicialização do site e integração com API

--- Public/js/sessao.js
+++ Public/js/sessao.js
@@ -26,16 +26,3 @@ function aguardarCadastro() {
     var divAguardar = document.getElementById("loaderCad");
     divAguardar.style.display = "flex";
 }
-
-
-function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
-
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.style.display = "flex";
-        divErrosLogin.innerHTML = texto;
-    }
-}
-
