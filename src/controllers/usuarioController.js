651257c5526d0e315ec545f8a7f27acd2f847de4
Rufino-0 <eli.rufino@sptech.school>
2024-07-26 01:48:59 -0300

    Inserção/Ateração - 26/07/24 - Nova estilização e validações para o cadastro do usuário, foram feitas usando display none e display flex para poder simular a troca de telas que a pagina tem para ficar mais dinamico para o usuário inserir seus dados, foram usadas 4 funções para todas as funcionalidades do cadastro

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -45,20 +45,23 @@ function autenticar(req, res) {
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
+    var sobrenome = req.body.sobrenomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
 
-    // Faça as validações dos valores
+    // Faça as validações dos valores - alteração 26/07/24
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
+    } else if (sobrenome == undefined) {
+        res.status(400).send("Seu sobrenome está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
     } else {
 
-        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js - alteração 26/07/24
+        usuarioModel.cadastrar(nome, sobrenome, email, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -9,14 +9,14 @@ function autenticar(email, senha) {
     return database.executar(instrucaoSql);
 }
   
-// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, email, senha) {
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql - alteração 26/07/24
+function cadastrar(nome, sobrenome, email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha) VALUES ('${nome}', '${email}', '${senha}');
+        INSERT INTO usuario (nome, sobrenome, email, senha) VALUES ('${nome}', '${sobrenome}', '${email}', '${senha}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
